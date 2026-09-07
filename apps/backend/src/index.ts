b5d5f4c60751b53cfe56d54340f3b595d7e56d4b
Вова Шимко <vovasimko@MacBook-Air-Vova-3.local>
2026-09-07 18:46:35 +0300

    preparation for deploy

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,10 +2,10 @@ import { statSync } from "node:fs";
 import app from "./app.js";
 import { startEventScheduler } from "./scheduler/eventScheduler";
 import { startTelegramBot } from "./bot/telegramBot";
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT || 3000);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-  startEventScheduler()
-  startTelegramBot()
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+  startEventScheduler();
+  startTelegramBot();
 });
